16e907277c252bcf3d44200f720109857e8e4941
Prabha Pattabiraman <prabhap@thoughtworks.com>
2014-04-09 13:39:34 +0530

    Prabha - Creating a private website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DanceSite::Application.config.secret_key_base = '781fca4ddd6729dc2c151d92b0820dcb376de54f56607e4e0d9f04e0d15ea23d06e8698a76fc63d4f3ba898299d22b7bec1b829a6c34b9cb16b209077eef5375'
