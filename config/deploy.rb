2b4fe7d9fa805ad3761c3c075bdbe6b273c32443
Prabha Pattabiraman <prabhap@thoughtworks.com>
2014-04-25 11:37:59 +0530

    chaning settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,9 +12,6 @@ set :repo_url, 'git@github.com:prabhap/my_portfolio.git'
 
 # Default value for :scm is :git
  set :scm, :git
- set :ssh_options, { :forward_agent => true }
- set :user, "haiitsmeprabha@gmail.com"
- set :scm_passphrase, "minmini26"
 # Default value for :format is :pretty
 # set :format, :pretty
 
