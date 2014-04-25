61e4bbaf436127fdcd2d68d1825293736a1c4979
Prabha Pattabiraman <prabhap@thoughtworks.com>
2014-04-25 11:30:23 +0530

    Prabha - setting ssh options

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :repo_url, 'git@github.com:prabhap/my_portfolio.git'
 
 # Default value for :scm is :git
  set :scm, :git
-
+ set :ssh_options, { :forward_agent => true }
 # Default value for :format is :pretty
 # set :format, :pretty
 
